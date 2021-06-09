f9f43946b8764b90b626dd23ed53df8ad22e6398
0201cs181063 <85599796+0201cs181063@users.noreply.github.com>
2021-06-09 18:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ from googletrans import Translator
 from pprint import pprint
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
-import gzip, pickle
+import pickle
+# import gzip, pickle
 
-# test = pickle.load(open('file.pkl', 'rb'))
-with gzip.open('file.pkl', 'rb') as ifp:
-    test=(pickle.load(ifp))
+test = pickle.load(open('file.pkl', 'rb'))
+# with gzip.open('file.pkl', 'rb') as ifp:
+#     test=(pickle.load(ifp))
 
 app = Flask(__name__)
 
