f5446d8c15671ccae8dff2b107ab7d121ab3f57d
0201cs181063 <85599796+0201cs181063@users.noreply.github.com>
2021-06-14 19:56:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,19 +102,19 @@ def home():
 #     h=result
 #     summarized_text1.append(result)
 #     summarized_text1 = []
-    url = "https://textanalysis-text-summarization.p.rapidapi.com/text-summarizer"
-    payload = "{
-#       \"url\": \"http://en.wikipedia.org/wiki/Automatic_summarization\",
-      \"text\": result,
-      \"sentnum\": 8
-    }"
-    headers = {
-        'content-type': "application/json",
-        'x-rapidapi-key': "3345d8c834msh75cec25088f47c7p140295jsndc688f6ab9e8",
-        'x-rapidapi-host': "textanalysis-text-summarization.p.rapidapi.com"
-        }
+#     url = "https://textanalysis-text-summarization.p.rapidapi.com/text-summarizer"
+#     payload = "{
+# #       \"url\": \"http://en.wikipedia.org/wiki/Automatic_summarization\",
+#       \"text\": result,
+#       \"sentnum\": 8
+#     }"
+#     headers = {
+#         'content-type': "application/json",
+#         'x-rapidapi-key': "3345d8c834msh75cec25088f47c7p140295jsndc688f6ab9e8",
+#         'x-rapidapi-host': "textanalysis-text-summarization.p.rapidapi.com"
+#         }
 
-    response = requests.request("POST", url, data=payload, headers=headers)
+#     response = requests.request("POST", url, data=payload, headers=headers)
 
 
 
