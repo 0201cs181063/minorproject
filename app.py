a72a60a1742381351f48cf38c8bb5e64d27a157c
0201cs181063 <85599796+0201cs181063@users.noreply.github.com>
2021-07-09 13:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,82 +16,82 @@ import regex
 import nltk
 # nltk.download('stopwords')
 
-def read_article(data):
+# def read_article(data):
 
-    article = data.split(". ")
-    sentences = []
-    for sentence in article:
-        review = regex.sub("[^A-Za-z0-9]",' ', sentence)
-        sentences.append(review.replace("[^a-zA-Z]", " ").split(" "))        
-    sentences.pop()     
-    return sentences
+#     article = data.split(". ")
+#     sentences = []
+#     for sentence in article:
+#         review = regex.sub("[^A-Za-z0-9]",' ', sentence)
+#         sentences.append(review.replace("[^a-zA-Z]", " ").split(" "))        
+#     sentences.pop()     
+#     return sentences
 
-def sentence_similarity(sent1, sent2, stopwords=None):
-    if stopwords is None:
-        stopwords = []
+# def sentence_similarity(sent1, sent2, stopwords=None):
+#     if stopwords is None:
+#         stopwords = []
  
-    sent1 = [w.lower() for w in sent1]
-    sent2 = [w.lower() for w in sent2]
+#     sent1 = [w.lower() for w in sent1]
+#     sent2 = [w.lower() for w in sent2]
  
-    all_words = list(set(sent1 + sent2))
+#     all_words = list(set(sent1 + sent2))
  
-    vector1 = [0] * len(all_words) #makes a vector of len all_words
-    vector2 = [0] * len(all_words)
+#     vector1 = [0] * len(all_words) #makes a vector of len all_words
+#     vector2 = [0] * len(all_words)
  
-    # build the vector for the first sentence
-    for w in sent1:
-        if w in stopwords:
-            continue
-        vector1[all_words.index(w)] += 1
+#     # build the vector for the first sentence
+#     for w in sent1:
+#         if w in stopwords:
+#             continue
+#         vector1[all_words.index(w)] += 1
  
-    # build the vector for the second sentence
-    for w in sent2:
-        if w in stopwords:
-            continue
-        vector2[all_words.index(w)] += 1
+#     # build the vector for the second sentence
+#     for w in sent2:
+#         if w in stopwords:
+#             continue
+#         vector2[all_words.index(w)] += 1
  
-    return 1 - nltk.cluster.util.cosine_distance(vector1, vector2)
+#     return 1 - nltk.cluster.util.cosine_distance(vector1, vector2)
 
-def build_similarity_matrix(sentences, stop_words):
-    # Create an empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+# def build_similarity_matrix(sentences, stop_words):
+#     # Create an empty similarity matrix
+#     similarity_matrix = np.zeros((len(sentences), len(sentences)))
  
-    for idx1 in range(len(sentences)):
-        for idx2 in range(len(sentences)):
-            if idx1 == idx2: #ignore if both are same sentences
-                continue 
-            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+#     for idx1 in range(len(sentences)):
+#         for idx2 in range(len(sentences)):
+#             if idx1 == idx2: #ignore if both are same sentences
+#                 continue 
+#             similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
 
-    return similarity_matrix
+#     return similarity_matrix
 
 
-def generate_summary(file_name, top_n=5):
-    stop_words = stopwords.words('english')
-    summarize_text = []
+# def generate_summary(file_name, top_n=5):
+#     stop_words = stopwords.words('english')
+#     summarize_text = []
 
-    # Step 1 - Read text anc split it
-    sentences =  read_article(file_name)
+#     # Step 1 - Read text anc split it
+#     sentences =  read_article(file_name)
     
-    # Step 2 - Generate Similary Martix across sentences
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+#     # Step 2 - Generate Similary Martix across sentences
+#     sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
 
-    # Step 3 - Rank sentences in similarity martix
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-    scores = nx.pagerank(sentence_similarity_graph)
+#     # Step 3 - Rank sentences in similarity martix
+#     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+#     scores = nx.pagerank(sentence_similarity_graph)
 
-    # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-#     print("\n\n---------------\nIndexes of top ranked_sentence order are ", ranked_sentence)    
+#     # Step 4 - Sort the rank and pick top sentences
+#     ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+# #     print("\n\n---------------\nIndexes of top ranked_sentence order are ", ranked_sentence)    
 
-    for i in range(top_n):
-      summarize_text.append(" ".join(ranked_sentence[i][1]))
+#     for i in range(top_n):
+#       summarize_text.append(" ".join(ranked_sentence[i][1]))
 
-    # Step 5 - Offcourse, output the summarize texr
-    # print("\n")
-    # print("*"*140)
-    # print("\n\nSUMMARY: \n---------\n\n", ". ".join(summarize_text))
-    a = ". ".join(summarize_text)
-    return a
+#     # Step 5 - Offcourse, output the summarize texr
+#     # print("\n")
+#     # print("*"*140)
+#     # print("\n\nSUMMARY: \n---------\n\n", ". ".join(summarize_text))
+#     a = ". ".join(summarize_text)
+#     return a
 
 
 app = Flask(__name__)
